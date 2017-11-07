04ec6008d8095231f10e3a7c1086a1d282f1cf38
PatrykMaternicki <patryk.maternicki@gmail.com>
2017-11-07 17:08:03 +0100

    final view main site

---
+++ JS/googleMapsApi.js
@@ -0,0 +1,215 @@
+
+document.addEventListener("DOMContentLoaded", initMap);
+function lol(){
+  console.log("COS");
+}
+
+var map;
+function initMap() {
+		var localization = new google.maps.LatLng(53.777,20.487);
+
+			// Styl mapy
+      var map_style= [
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    }
+];
+
+
+
+
+
+				var option_map = {
+					zoom: 15,
+					center: localization,
+					mapTypeControl: false,
+					navigationControl:false
+				};
+				map = new google.maps.Map(document.getElementById("map"), option_map);
+				map.setOptions({styles: map_style});
+
+
+
+				//Właścuwiści markera
+				var icon_size = new google.maps.Size(64,64);
+				var new_point_start = new google.maps.Point(0,0);
+				var new_point_catch = new google.maps.Point(16,16);
+				var marker_image =  "./Image/marker.png";
+				// Stworzenie nowego markera
+				var marker = new google.maps.MarkerImage(marker_image,icon_size,new_point_start, new_point_catch);
+
+				// stworzenie markera właściwości
+				var point  = new google.maps.LatLng(53.775,20.486);
+				var option_marker =
+				{
+					position: point,
+					map: map,
+					title: "",
+					icon: marker
+				}
+
+				//Inicjacja API
+				var gmp_marker = new google.maps.Marker(option_marker);
+}
