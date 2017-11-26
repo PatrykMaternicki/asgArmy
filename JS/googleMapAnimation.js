e3e154f0b2ceda1fb7ddb277208211f39c0698e5
PatrykMaternicki <patryk.maternicki@gmail.com>
2017-11-26 08:48:46 +0100

    Animation: Animate google-map

--- JS/googleMapAnimation.js
+++ JS/googleMapAnimation.js
@@ -10,11 +10,15 @@ function switchDisplayMap(){
 }
 
 function switchToAddress(){
-  removeMap()
+  removeMap();
+  backAddress();
 }
 
 function changeSizeMap(element,percentWidth){
   let percentValue = Number.parseInt(splitValue(percentWidth,"%"));
+    if (mapHad50Percent()){
+      $('#map-checker-button--return').css("display","block");
+    }
     if (mapHad100Percent(percentValue)){
       $('#map').css("width","100%");
       return;
@@ -40,12 +44,20 @@ function decreaseValue(transformValue){
 function mapHad100Percent(percentValue){
   return percentValue >= 100;
 }
+function mapHad50Percent(percentValue){
+  return percentValue => 50;
+}
+
+function transformIsEqual0(value){
+   return value == 0;
+}
 
 function transformAddressContainer(transformValue){
   if (transformIsEqual100(transformValue)){
     return;
   }
   let newValue = decreaseValue(transformValue);
+  $('#address').data("transform",newValue);
   $('#address').css("transform","translateX(" + newValue + "%)");
 
   window.requestAnimationFrame(()=>{this.transformAddressContainer(newValue);})
@@ -69,4 +81,17 @@ function removeMap(){
 
 function mapHadWidth0Percent(value){
   return value <= 0;
+}
+
+function backAddress(){
+  console.log('HALO');
+  let element = $('#address');
+  console.log(element.data());
+  if (transformIsEqual0(element.data("transform"))){
+    return;
+  }
+  let newValue = increaseValue(element.data("transform"));
+  element.css("transform","translateX(" + newValue + "%)");
+  element.data("transform",newValue);
+  window.requestAnimationFrame(()=>this.backAddress())
 }
